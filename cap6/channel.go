9d9afae684aab42964cd7c28c6b494914abab314
Frederico Benevides <fredbene@gmail.com>
2015-07-23 18:00:44 -0300

    terminado o cap6

---
+++ cap6/channel.go
@@ -0,0 +1,71 @@
+// This sample program demonstrates how to use the atomic
+// package functions Store and Load to provide safe access
+// to numeric types.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// wg is used to wait for the program to finish
+var wg sync.WaitGroup
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	// Create an unbuffered channel.
+	court := make(chan int)
+
+	// Add a count of two, one for each goroutine.
+	wg.Add(2)
+
+	// Launch two players.
+	go player("Nadal", court)
+	go player("Djokovic", court)
+
+	// Start the set
+	fmt.Println("Iniciando court")
+	court <- 1
+
+	// Wait for the game to finish.
+	wg.Wait()
+}
+
+// player simulates a person playing the game of tennis.
+func player(name string, court chan int) {
+	// Schedule the call to Done to tell main we are done
+	defer wg.Done()
+
+	for {
+		// Wait for the ball to be hit back to us.
+		ball, ok := <-court
+		if !ok {
+			// If the channel was closed we won.
+			fmt.Printf("Player %s Won\n", name)
+			return
+		}
+
+		// Pick a random number and see if we miss the ball.
+		n := rand.Intn(100)
+		if n%13 == 0 {
+			fmt.Printf("Player %s Missed\n", name)
+
+			// Close the channel to signal we lost.
+			close(court)
+			return
+		}
+
+		// Display and then increment the hit count by one.
+		fmt.Printf("Player %s Hit %d\n", name, ball)
+		ball++
+
+		// Hit the ball back to the opposing player.
+		court <- ball
+	}
+
+}
